88e334f3c88d227a7fe009cf018786c0fe3db4e8
ashlinrajangabriel <71674232+ashlinrajangabriel@users.noreply.github.com>
2021-11-27 19:44:10 +0530

    Update Performers.py

--- Performers.py
+++ Performers.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 13 10:57:58 2021
+
+@author: scarl
+"""
+
 import dash
 import dash_html_components as html
 import plotly.graph_objects as go
@@ -8,7 +15,8 @@ import dash_bootstrap_components as dbc
 import numpy as np
 import dash_table as dt
 import dash_table
-
+import colorlover
+import dash_html_components as html
 from nsetools import Nse
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])   
 
@@ -19,34 +27,99 @@ losers=pd.DataFrame(nse.get_top_losers())
 winners=pd.DataFrame(nse.get_top_gainers())
 columns = losers.columns
 
+#Pricedifference
+losers['HighLowDifference'] = losers['ltp']-losers['openPrice']
+winners['HighLowDifference'] = winners['ltp']-winners['openPrice']
+losers.drop('series', axis=1, inplace=True)
+winners.drop('series', axis=1, inplace=True)
+
+PLOTLY_LOGO = "https://www.pngkey.com/png/full/773-7732073_multichoice-is-delighted-todeliver-the-best-movies-m.png"
+
+navbar = dbc.Navbar( id = 'navbar', children = [
+    dbc.Row([
+        dbc.Col(html.Img(src = PLOTLY_LOGO, height = "40px")),
+
+        dbc.Col(
+             
+            dbc.NavbarBrand(" Stock Crash Dashboard", style = {'margin-left': '130px','text-align':'center','color':'white', 'fontSize':'25px','fontFamily':'Times New Roman'}
+                            )
+            
+            ),
+        
+        dbc.Col(
+            dbc.NavLink("Performers",href='/',active='exact', style = {'margin-left': '130px','text-align':'center','color':'white', 'fontSize':'20px','fontFamily':'Times New Roman'}) 
+            )
+        
+        
+        ],align = "center"
+       ),
+    
+    
+    ], color = '#000000')
+
+
 #INfluencers are positive and negative win and lossers
 def Personnel(influencers):
   for i in range(0,len(influencers)):
     print(influencers.symbol[i],influencers.lowPrice[i])
 
-
+###############################################
 body_app = dbc.Container([
- dbc.Col([  
-   dash_table.DataTable(
+    html.Br(),
+    html.Hr(),
+    html.H3("Exceeding performers"),
+dbc.Row([ 
+dash_table.DataTable(
     id='table',
     columns=[{"name": i, "id": i} for i in winners.columns],
     data=winners.to_dict('records')
-    )
-    ]),
- dbc.Col([  
-   dash_table.DataTable(
+    ),
+   html.Br()
+   ])
+     ,     
+
+ html.Br(),
+  html.Br(),
+  
+   html.H3("Exceeding Losers"),
+dbc.Row([ 
+dash_table.DataTable(
     id='table1',
-    columns=[{"name": i, "id": i} for i in losers.columns],
-    data=losers.to_dict('records')
+    data=losers.to_dict('records'),
+    sort_action='native',
+    columns=[
+        {"name": i, "id": i} for i in losers.columns
+    ],
+    style_data_conditional=[
+        {
+            'if': {
+                'filter_query': '{netPrice} < 0',
+                'column_id': 'netPrice'
+            },
+            'color': 'tomato',
+            'fontWeight': 'bold'
+        },
+        {
+            'if': {
+                'filter_query': '{netPrice} > 0',
+                'column_id': 'netPrice'
+            },
+            'textDecoration': 'underline'
+        }
+    ]
+
     )
-    ])
+  ]
+            )
         
     
-   ])
-   
+   ],style = {'margin-left': '10px'})
 
 
-app.layout =  html.Div(id='parent', children = [body_app])
+    
+
+
+app.layout =  html.Div(id='parent', children = [navbar,body_app])
 
 
 
